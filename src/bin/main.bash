0b6d0f8e130925e35e060432d3f9f5dfd37f8203
Kreyren <KostWarCZE@RiXotStudio.cz>
2020-02-03 08:48:17 +0000

    fgjghfj

--- src/bin/main.bash
+++ src/bin/main.bash
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
 printf '%s\n' "ping"
+
+# Argument management
+while [ $# -ge 1 ]; do case $1 in 
+    --help|-h|help)
+        fixme "Help message here"
+        shift 1
+    ;;
+    *)
+        die 2 "FIXME: Syntax error"
+esac; done
