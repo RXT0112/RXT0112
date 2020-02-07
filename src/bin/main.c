7425043f68898e3f5a38d05fffaf1e5f4249809c
Kreyren <KostWarCZE@RiXotStudio.cz>
2020-02-07 12:37:25 +0000

    fixes

--- src/bin/main.c
+++ src/bin/main.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
+// FIXME: Does not build on clang
+// FIXME-QA: Outputs warnings
+
 int main(int argc, char ** argv[]) {
-
-
-
-	if(argc < 2)
-	{
-		printf("\nArgument(s) required, use -h for display help\n");
+	if(argc < 2) {
+		// FIXME: Add better help message
+		// FIXME: Source from po depending on language used
+		printf("%s\n", "Argument(s) required, use -h for display help");
+		return 2;
+	} else if(strcmp(argv[1], "-h") == 0) {
+		printf("FIXME: %s\n", "Add optionsi");
+		return 1;
+	}	else {
+		printf("FATAL: %s\n", "Unexpected happend while processing arguments");
+		return 256;
 	}
-	else if(strcmp(argv[1], "-h") == 0)
-	{
-		printf("\n Fixme: Add optionsi\n");
-	}
-	else
-	{
-		printf("\nuwu");
-	}
-
-
-	return 0;
 }
